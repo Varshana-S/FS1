950d77106b0bc2b2a523fc0107dfd268bbbef252
Varshana-S <varshana351@gmail.com>
2025-06-25 10:09:07 +0530

    backend update

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 export const connectDB=async()=>{
     try {
-        await mongoose.connect('mongodb://localhost:27017/students')
+        await mongoose.connect(process.env.MONGO_URL)
         console.log('MongoDB Connected')
     } catch (error) {
         console.log(error);
